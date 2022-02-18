1ad9560c8d04f5abd766393ab6f92f2f8ed09999
Eduardo Pelaez Alvarado <eduardopelaez@pm.me>
2022-02-17 19:46:58 -0800

    feat: migrate db after adding Event

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2022_02_17_141545) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "events", force: :cascade do |t|
+    t.string "name"
+    t.string "date"
+    t.string "start_time"
+    t.string "end_time"
+    t.text "description"
+    t.boolean "private"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
 end
