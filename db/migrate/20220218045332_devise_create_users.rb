37c8302163b3a21c1829e065a11d1a068b6e76f0
Eduardo Pelaez Alvarado <eduardopelaez@pm.me>
2022-02-18 00:41:17 -0800

    feat: add username to devise_create_users migration

--- db/migrate/20220218045332_devise_create_users.rb
+++ db/migrate/20220218045332_devise_create_users.rb
@@ -3,6 +3,8 @@
 class DeviseCreateUsers < ActiveRecord::Migration[6.1]
   def change
     create_table :users do |t|
+      t.string :username, null: false, default: ""
+
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
@@ -13,6 +15,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.1]
       t.timestamps null: false
     end
 
+    add_index :users, :username,                unique: true
     add_index :users, :email,                unique: true
   end
 end
