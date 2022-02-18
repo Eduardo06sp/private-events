58f001f162ba2d06d26525f94f089671ec5b38fb
Eduardo Pelaez Alvarado <eduardopelaez@pm.me>
2022-02-18 10:51:34 -0800

    feat: associate events with their creators

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :rememberable
 
+  has_many :created_events, foreign_key: :creator_id, class_name: 'Event',
+           dependent: :destroy
+
   validates :email, uniqueness: true
   validates :username, uniqueness: true
 end
