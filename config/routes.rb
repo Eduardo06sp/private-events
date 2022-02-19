99db50baa74b9cc1815ec0dce1405e0143eaef1b
Eduardo Pelaez Alvarado <eduardopelaez@pm.me>
2022-02-18 22:30:16 -0800

    feat: add show route to Users

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   get 'events/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
+  resources :users, only: :show
   root to: 'events#index'
 end
