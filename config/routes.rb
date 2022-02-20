6840b2ebb1dc1b17fa51b54647304c13cbbf274e
Eduardo Pelaez Alvarado <eduardopelaez@pm.me>
2022-02-19 23:20:12 -0800

    fix: fix typo in routes config for devise controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: { session: 'users/sessions' }
+  devise_for :users, controllers: { sessions: 'users/sessions' }
   devise_scope :user do
     get '/users/:id' => 'users/sessions#show'
   end
