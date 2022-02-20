1a6d2fddabf6fa38923ebaa15f5f87898be73421
Eduardo Pelaez Alvarado <eduardopelaez@pm.me>
2022-02-19 19:41:19 -0800

    feat: add new action with proper route for Events

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
     get '/users/:id' => 'users/sessions#show'
   end
 
-  resources :events, only: [:index]
+  resources :events, only: [:index, :new]
   root to: 'events#index'
 end
