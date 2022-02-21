18dc4df7e5545975426815d3683ae678914657d3
Eduardo Pelaez Alvarado <eduardopelaez@pm.me>
2022-02-20 18:42:40 -0800

    feat: specify foreign key table in CreateInvitations migration

--- db/migrate/20220221022221_create_invitations.rb
+++ db/migrate/20220221022221_create_invitations.rb
@@ -1,8 +1,8 @@
 class CreateInvitations < ActiveRecord::Migration[6.1]
   def change
     create_table :invitations do |t|
-      t.references :attendee, null: false, foreign_key: true
-      t.references :attended_events, null: false, foreign_key: true
+      t.references :attendee, null: false, foreign_key: {to_table: :users}
+      t.references :attended_events, null: false, foreign_key: {to_table: :events}
 
       t.timestamps
     end
