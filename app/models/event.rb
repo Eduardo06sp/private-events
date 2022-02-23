4e240f0e6619482c46a0edbfe14d307b16524e7d
Eduardo Pelaez Alvarado <eduardopelaez@pm.me>
2022-02-22 20:02:20 -0800

    feat: define foreign keys for Invitations within User and Event

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,5 +2,5 @@ class Event < ApplicationRecord
   belongs_to :creator, class_name: 'User'
 
   has_many :attendees, through: :invitations, source: :attendee
-  has_many :invitations
+  has_many :invitations, foreign_key: :attended_event_id
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
   has_many :attended_events, through: :invitations
   has_many :created_events, foreign_key: :creator_id, class_name: 'Event',
            dependent: :destroy
-  has_many :invitations
+  has_many :invitations, foreign_key: :attendee_id
 
   validates :email, uniqueness: true
   validates :username, uniqueness: true
