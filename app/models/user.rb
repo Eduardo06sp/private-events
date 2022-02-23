aef19f1f0bc5ac286184aaf7277ca82f35dc0967
Eduardo Pelaez Alvarado <eduardopelaez@pm.me>
2022-02-22 20:06:07 -0800

    fix: reorder has_many through Invitations association in User and Event models

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,10 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :rememberable
 
-  has_many :attended_events, through: :invitations
   has_many :created_events, foreign_key: :creator_id, class_name: 'Event',
            dependent: :destroy
   has_many :invitations, foreign_key: :attendee_id
+  has_many :attended_events, through: :invitations
 
   validates :email, uniqueness: true
   validates :username, uniqueness: true
