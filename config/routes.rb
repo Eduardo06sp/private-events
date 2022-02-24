b4c54e64225e101f076127b522556ade986bed93
Eduardo Pelaez Alvarado <eduardopelaez@pm.me>
2022-02-23 22:11:12 -0800

    feat: add button and route to allow signing out

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { sessions: 'users/sessions' }
   devise_scope :user do
     get '/users/:id' => 'users/sessions#show'
+    post '/users/sign_out' => 'users/sessions#destroy'
   end
 
   resources :events, only: [:index, :new, :create, :show]
