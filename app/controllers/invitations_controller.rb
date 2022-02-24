1b2b6a9620375622e7ff902e261c36fceff252e5
Eduardo Pelaez Alvarado <eduardopelaez@pm.me>
2022-02-23 21:09:07 -0800

    feat: add create action for Invitations

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -1,2 +1,14 @@
 class InvitationsController < ApplicationController
+  def create
+    current_event = Event.find(params[:event_id])
+    @invitation = current_user.invitations.build(attended_event_id: current_event.id)
+
+    if current_user.save
+      flash[:notice] = 'SUCCESS'
+      redirect_back fallback_location: '/events'
+    else
+      flash[:notice] = 'FAILURE'
+      redirect_back fallback_location: '/events'
+    end
+  end
 end
