682e7d54107eca9aef936e3bf884553112fdc3c0
Eduardo Pelaez Alvarado <eduardopelaez@pm.me>
2022-02-26 00:17:45 -0800

    feat: add Events end_date column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_26_080355) do
+ActiveRecord::Schema.define(version: 2022_02_26_081538) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2022_02_26_080355) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "creator_id", null: false
+    t.string "end_date"
     t.index ["creator_id"], name: "index_events_on_creator_id"
   end
 
