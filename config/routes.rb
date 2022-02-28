467ba7378dcfa955895847b11416a6101961aa84
Eduardo Pelaez Alvarado <eduardopelaez@pm.me>
2022-02-27 18:03:01 -0800

    feat: route newDevise User registrations to custom controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: { sessions: 'users/sessions' }
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
+  }
   devise_scope :user do
     get '/users/:id' => 'users/sessions#show'
     get '/users/sign_in' => 'users/sessions#create'
