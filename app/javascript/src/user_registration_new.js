b2cd30a12d9ddd20fa29180db12232ca94b54f22
Eduardo Pelaez Alvarado <eduardopelaez@pm.me>
2022-02-28 00:06:22 -0800

    feat: set default selection based on local user time zone

--- app/javascript/src/user_registration_new.js
+++ app/javascript/src/user_registration_new.js
@@ -12,3 +12,18 @@ const getTimeAndOffset = function() {
 const timeAndOffset = getTimeAndOffset();
 const time = timeAndOffset.slice(0, 8);
 const offset = timeAndOffset.slice(9);
+
+const timeZoneDropdown = document.getElementById('user_time_zone');
+const timeZoneOptions = Array.from(timeZoneDropdown.children);
+const offsetRegex = /GMT.*\d/g;
+
+const selectOffset = function() {
+  const matchingOffsetOption = timeZoneOptions.find( (option) => {
+    const currentOffset = option.textContent.match(offsetRegex)[0];
+    return currentOffset === offset;
+  });
+
+  timeZoneDropdown.value = matchingOffsetOption.value;
+};
+
+selectOffset();
