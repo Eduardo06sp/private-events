8ef81fe6d6dd4b1156ce7daaa710058a910a672e
Eduardo Pelaez Alvarado <eduardopelaez@pm.me>
2022-02-27 22:57:42 -0800

    refactor: use function to return time/offset

--- app/javascript/src/user_registration_new.js
+++ app/javascript/src/user_registration_new.js
@@ -1,10 +1,14 @@
-const currentDate = Date.now();
-const timeAndOffsetFormat = new Intl.DateTimeFormat('en-US', {
-  hour: '2-digit',
-  minute: '2-digit',
-  timeZoneName: 'longOffset'
-});
-const timeAndOffset = timeAndOffsetFormat.format(currentDate);
+const getTimeAndOffset = function() {
+  const currentDate = Date.now();
+  const timeAndOffsetFormat = new Intl.DateTimeFormat('en-US', {
+    hour: '2-digit',
+    minute: '2-digit',
+    timeZoneName: 'longOffset'
+  });
 
+  return timeAndOffsetFormat.format(currentDate);
+};
+
+const timeAndOffset = getTimeAndOffset();
 const time = timeAndOffset.slice(0, 8);
-let offset = timeAndOffset.slice(9);
+const offset = timeAndOffset.slice(9);
