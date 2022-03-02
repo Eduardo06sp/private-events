528923d899195f40b4a2f2ebc6b0acc70e28e341
Eduardo Pelaez Alvarado <eduardopelaez@pm.me>
2022-03-02 06:16:45 -0800

    feat: dynamically display selected offset time

--- app/javascript/src/user_registration_new.js
+++ app/javascript/src/user_registration_new.js
@@ -30,4 +30,65 @@ const selectOffset = function() {
   }
 };
 
-selectOffset();
+selectOffset(); // this line & above needs refactoring too
+
+const updateTimeSpan = function() {
+  const currentTimeSpan = document.getElementById('dynamic_time');
+  let selectedOptionIndex = timeZoneDropdown.selectedIndex;
+  let selectedTimeZoneOption = timeZoneDropdown.children[selectedOptionIndex];
+  let selectedOffset = selectedTimeZoneOption
+    .textContent
+    .match(offsetRegex)[0];
+
+  const offsetNumberRegex = /[+-]*\d+/g;
+  let selectedOffsetNumber = selectedTimeZoneOption
+    .textContent
+    .match(offsetNumberRegex);
+  let localOffsetNumber = offset.match(offsetNumberRegex);
+
+  let currentDate = new Date();
+  const formatter = new Intl.DateTimeFormat('en-US', {
+    hour: '2-digit',
+    minute: '2-digit',
+  });
+
+  // make selected minutes negative if the hours are negative
+  let minute;
+  if (parseInt(selectedOffsetNumber[0]) < 0) {
+    minute = -(parseInt(selectedOffsetNumber[1]));
+  } else {
+    minute = parseInt(selectedOffsetNumber[1]);
+  }
+
+  // if localOffset x is negative
+  if (parseInt(localOffsetNumber[0]) < 0) {
+    // UTC is x hours AHEAD, so add positive x local offset to UTC hour
+    // to convert user local time to UTC-equivalent
+    // then subtract the selected offset
+    // to represent current time in selected offset, regardless of local time zone
+    // set this as the new UTC time which gets converted to local time
+    // but is now properly offset (according to the selected time zone)
+    currentDate.setUTCHours(
+      currentDate.getUTCHours() +
+      Math.abs(parseInt(localOffsetNumber[0])) +
+      parseInt(selectedOffsetNumber[0]),
+      currentDate.getUTCMinutes() +
+      Math.abs(parseInt(localOffsetNumber[1])) +
+      minute);
+  } else {
+    currentDate.setUTCHours(
+      currentDate.getUTCHours() -
+      Math.abs(parseInt(localOffsetNumber[0])) +
+      parseInt(selectedOffsetNumber[0]),
+      currentDate.getUTCMinutes() -
+      Math.abs(parseInt(localOffsetNumber[1])) +
+      minute);
+  }
+
+  currentTimeSpan.textContent = `${formatter.format(currentDate)} (${selectedOffset})`;
+
+  /* whole function needs refactoring */
+  // change let to constants?
+};
+
+timeZoneDropdown.addEventListener('change', updateTimeSpan);
