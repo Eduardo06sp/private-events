b5d15152149a09fc5bd4fc6ee01d91fcbc3336a0
Eduardo Pelaez Alvarado <eduardopelaez@pm.me>
2022-03-06 21:06:04 -0800

    feat: set User time zone around all actions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  around_action :set_time_zone, if: :current_user
+
+  private
+
+  def set_time_zone
+    user_time_zone = current_user.time_zone
+    Time.use_zone(user_time_zone) { yield }
+  end
 end
