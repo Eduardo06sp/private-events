227a23d843178bcb4ed2aa09a75c7237072dff50
Eduardo Pelaez Alvarado <eduardopelaez@pm.me>
2022-03-07 18:26:37 -0800

    refactor: use Event class methods to display past and upcoming events

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,4 +3,12 @@ class Event < ApplicationRecord
 
   has_many :invitations, foreign_key: :attended_event_id
   has_many :attendees, through: :invitations, source: :attendee
+
+  def self.upcoming
+    Event.joins(:creator).where('events.start_timewithzone >= ?', Time.current)
+  end
+
+  def self.past
+    Event.joins(:creator).where('events.end_timewithzone < ?', Time.current)
+  end
 end
